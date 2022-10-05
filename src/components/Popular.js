4c7a3bf422db7ec44e83d5bc5e58c02297ed99d1
Francis Maina <sebastianfrancix@gmail.com>
2022-10-05 17:55:14 +0300

    fetches popular recipes from the api

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,4 +1,14 @@
 function Popular() {
+  //fetch 10 popular recipes from the api
+  const popularRecipes = async () => {
+    // API key is contained in the .env file
+    const api = await fetch(
+      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.SPOONACULAR_API_KEY}&number=10`
+    );
+    const data = await api.json();
+    console.log(data);
+  };
+
   return <div>Popular</div>;
 }
 
