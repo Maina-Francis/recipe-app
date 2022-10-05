196854a719a002f12f6d5ee47ed2b2431bd83501
Francis Maina <sebastianfrancix@gmail.com>
2022-10-05 17:45:16 +0300

    adds the components and pages

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import Popular from "../components/Popular";
+import Veggies from "../components/Veggies";
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <Veggies />
+      <Popular />
+    </div>
+  );
+}
+
+export default Home;
