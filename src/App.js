b74f9ab886e81dda1116274c8c9b82f32aceb81b
Francis Maina <sebastianfrancix@gmail.com>
2022-10-07 14:50:00 +0300

    made changes to the api key

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import Pages from "./pages/Pages";
 import React from "react";
+import "./key";
 
 function App() {
+  const API_KEY = "8e7ccc310f4b29da4ed1741a7382c90d";
+  const API_ID = "f923a481";
+  const url = `https://api.edamam.com/search?q=chicken&app_id=${API_ID}&app_key=${API_KEY}&&health=alcohol-free`;
+
+  //get recipes function
+  async function getRecipes() {
+    let results = await fetch(url)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+    // .catch((err) => console.error(err));
+
+    // console.log(results.data);
+  }
+
   return (
     <div className="App">
-      <h1>Hello React</h1>
-      <Pages />
+      <h1 onClick={getRecipes}>Hello React</h1>
     </div>
   );
 }
