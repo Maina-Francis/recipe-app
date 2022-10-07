45d37421d2e7751e53cba63442037a28a3a79f56
Francis Maina <sebastianfrancix@gmail.com>
2022-10-07 15:07:56 +0300

    adds the search ingredient input form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
-import "./key";
+import { useState } from "react";
 
 function App() {
   const API_KEY = "8e7ccc310f4b29da4ed1741a7382c90d";
   const API_ID = "f923a481";
-  const url = `https://api.edamam.com/search?q=chicken&app_id=${API_ID}&app_key=${API_KEY}&&health=alcohol-free`;
+
+  const url = `https://api.edamam.com/search?q={search}&app_id=${API_ID}&app_key=${API_KEY}&&health=alcohol-free`;
+
+  //useState Hook
+  const [search, setSearch] = useState("");
 
   //get recipes function
   async function getRecipes() {
@@ -19,6 +23,14 @@ function App() {
   return (
     <div className="app">
       <h1 onClick={getRecipes}>Food Recipe Galore🍕</h1>
+      <form className="search">
+        <input
+          type="text"
+          placeholder="Search Ingredient"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </form>
     </div>
   );
 }
