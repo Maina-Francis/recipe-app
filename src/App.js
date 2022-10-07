39ff313a0c8db2a7bbc12ee973883fa989764411
Francis Maina <sebastianfrancix@gmail.com>
2022-10-07 20:03:36 +0300

    adds the dropdown options

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import Axios from "axios";
 import Recipes from "./components/Recipes";
 
 function App() {
-  const API_KEY = "11f1338bfc319891045d4a7721dabd03";
-  const API_ID = "0a97fde9";
-
-  const url = `https://api.edamam.com/search?q={search}&app_id=${API_ID}&app_key=${API_KEY}&health=alcohol-free`;
-
   //useState Hook
   const [search, setSearch] = useState("");
   const [recipes, setRecipes] = useState([]);
+  const [healthLabel, sethealthLabel] = useState("gluten-free");
+
+  const API_KEY = "11f1338bfc319891045d4a7721dabd03";
+  const API_ID = "0a97fde9";
+
+  const url = `https://api.edamam.com/search?q={search}&app_id=${API_ID}&app_key=${API_KEY}&health=${healthLabel}`;
 
   //get recipes function
   // ***tried but got Unhandled Rejection (TypeError): Failed to fetch***
@@ -54,6 +55,121 @@ function App() {
           onChange={(e) => setSearch(e.target.value)}
         />
         <input className="submit" type="submit" value="Search" />
+
+        {/* dropdown */}
+        <select className="healthLabels">
+          <option
+            onClick={() => {
+              sethealthLabel("gluten-free");
+            }}
+          >
+            Gluten-Free
+          </option>
+
+          <option
+            onClick={() => {
+              sethealthLabel("vegan");
+            }}
+          >
+            Vegan
+          </option>
+
+          <option
+            onClick={() => {
+              sethealthLabel("vegetarian");
+            }}
+          >
+            Vegetarian
+          </option>
+
+          <option
+            onClick={() => {
+              sethealthLabel("paleo");
+            }}
+          >
+            Paleo
+          </option>
+
+          <option
+            onClick={() => {
+              sethealthLabel("dairy-free");
+            }}
+          >
+            Dairy-Free
+          </option>
+
+          <option
+            onClick={() => {
+              sethealthLabel("wheat-free");
+            }}
+          >
+            Wheat-Free
+          </option>
+
+          <option
+            onClick={() => {
+              sethealthLabel("fat-free");
+            }}
+          >
+            Fat-Free
+          </option>
+
+          <option
+            onClick={() => {
+              sethealthLabel("low-sugar");
+            }}
+          >
+            Low-Sugar
+          </option>
+
+          <option
+            onClick={() => {
+              sethealthLabel("egg-free");
+            }}
+          >
+            Egg-Free
+          </option>
+
+          <option
+            onClick={() => {
+              sethealthLabel("peanut-free");
+            }}
+          >
+            Peanut-Free
+          </option>
+
+          <option
+            onClick={() => {
+              sethealthLabel("tree-nut-free");
+            }}
+          >
+            Tree-Nut Free
+          </option>
+
+          <option
+            onClick={() => {
+              sethealthLabel("soy-free");
+            }}
+          >
+            Soy-Free
+          </option>
+
+          <option
+            onClick={() => {
+              sethealthLabel("fish-free");
+            }}
+          >
+            Fish-Free
+          </option>
+
+          <option
+            onClick={() => {
+              sethealthLabel("shellfish-free");
+            }}
+          >
+            Shellfish-Free
+          </option>
+        </select>
       </form>
 
       <div className="app-recipes">
