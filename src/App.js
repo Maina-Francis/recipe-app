5002340dfc4953458b15446a429713562d653770
Francis Maina <sebastianfrancix@gmail.com>
2022-10-08 09:57:17 +0300

    modified the fetch function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import RecipeCard from "./components/RecipeCard";
+import SearchBar from "./components/SearchBar";
+
+const url = "www.themealdb.com/api/json/v1/1/search.php?s=";
 
 function App() {
-  return <div className="container">App</div>;
+  //useState Hook
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  //fetch data from the api function
+  const getRecipes = async () => {
+    setLoading(true);
+    const searchUrl = url + searchTerm;
+    const response = await fetch(searchUrl);
+    const data = await response.json();
+    setRecipes(data.meals);
+    setLoading(false);
+  };
+  return (
+    <div className="container">
+      <h1>Food Galore 🍕 </h1>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/RecipeCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function RecipeCard(props) {
+  return <div>RecipeCard</div>;
+}
+
+export default RecipeCard;
